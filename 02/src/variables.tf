6fb218cf1bf525dc34b584b31248efe78df37791
Sibrael <108219535+sibrael@users.noreply.github.com>
2024-09-04 11:01:13 +0500

    Update variables.tf

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -6,22 +6,37 @@ variable "token" {
 
 variable "cloud_id" {
   type        = string
+  default = "b1g40slsj9pvhvgo3r5b"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "b1g9rgfing2h10u1r4qp"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "default_zone" {
+variable "default_zone_a" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+
+variable "default_zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "default_cidr_a" {
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["10.0.3.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "default_cidr_b" {
+  type        = list(string)
+  default     = ["10.0.4.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -31,11 +46,9 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
-
 ###ssh vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
 }
