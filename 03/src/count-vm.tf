207dd8e053eaacce9cc774f8976cd0c700bb769c
Sibrael <108219535+sibrael@users.noreply.github.com>
2025-01-08 15:50:11 +0500

    Update count-vm.tf

--- 03/src/count-vm.tf
+++ 03/src/count-vm.tf
@@ -6,8 +6,8 @@ data "yandex_compute_image" "ubuntu-2004-lts" {
 resource "yandex_compute_instance" "example" {
   count = 2
 
-  name        = "netology-develop-platform-web-${count.index}" #Имя ВМ в облачной консоли
-  hostname    = "netology-develop-platform-web-${count.index}" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
+  name        = "netology-develop-platform-web-${count.index +1}" #Имя ВМ в облачной консоли
+  hostname    = "netology-develop-platform-web-${count.index +1}" #формирует FDQN имя хоста, без hostname будет сгенрировано случаное имя.
   platform_id = "standard-v1"
 
   depends_on = [
@@ -38,6 +38,7 @@ resource "yandex_compute_instance" "example" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     security_group_ids = [yandex_vpc_security_group.example.id]
+    nat = true
   }
   allow_stopping_for_update = true
 }
