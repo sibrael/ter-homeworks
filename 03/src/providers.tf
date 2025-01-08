0de9bb7353d5f0c5e16c074f26eceeeae3f1516f
Sibrael <108219535+sibrael@users.noreply.github.com>
2025-01-08 07:59:52 +0500

    Update providers.tf

--- 03/src/providers.tf
+++ 03/src/providers.tf
@@ -4,12 +4,13 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = "~>1.8.4"
+  required_version = ">=1.5"
 }
 
 provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
+  # token     = var.token
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.default_zone_a
+  service_account_key_file = file("~/.authorized_key.json")
 }
