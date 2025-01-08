93f9e6a30813ce1793528c06ebf6697434802b5c
Sibrael <108219535+sibrael@users.noreply.github.com>
2025-01-08 08:01:34 +0500

    Update variables.tf

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,32 +1,117 @@
 ###cloud vars
 variable "token" {
   type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  description = "---"
 }
 
 variable "cloud_id" {
   type        = string
+  default = "b1g40slsj9pvhvgo3r5b"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default = "b1g9rgfing2h10u1r4qp"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "default_zone" {
+variable "default_zone_a" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+
+variable "default_zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "default_cidr_a" {
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["10.0.3.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "default_cidr_b" {
+  type        = list(string)
+  default     = ["10.0.4.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
 variable "vpc_name" {
   type        = string
   default     = "develop"
-  description = "VPC network&subnet name"
+  description = "VPC network & subnet name"
+}
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZWQyNTUxOQAAACB6ChslbM8xTWeiiYggxcjE85HTmh1YIiDdKzFvTL+akgAAAJAtvo/2Lb6P9gAAAAtzc2gtZWQyNTUxOQAAACB6ChslbM8xTWeiiYggxcjE85HTmh1YIiDdKzFvTL+akgAAAEDUzh4tPY/lxuwac+91HA5cNlG+fod7WCPtC0LN>  description = "ssh-keygen -t ed25519"
+}
+
+variable "each_vm" {
+  type = list(object({
+      vm_name=string,
+      cpu=number,
+      ram=number,
+      disk_volume=number
+      core_fraction=number
+      }))
+  default = [
+    {
+      vm_name = "main",
+      cpu = 2,
+      ram = 2,
+      disk_volume = 10
+      core_fraction = 5
+    },
+    {
+      vm_name = "replica",
+      cpu = 2,
+      ram = 4,
+      disk_volume = 12
+      core_fraction = 5
+    }
+  ]
+}
+
+variable "storages" {
+  type        = list(string)
+  default     = ["1", "2", "3"]
+}
+
+variable "storage" {
+  type        = string
+  default     = "netology-develop-platform-storage"
+  description = "storage name"
+}
+
+variable "vms_resources" {
+  type=map(object({
+    cores = number
+    memory = number
+    core_fraction = number
+    hdd_size = number
+    hdd_type = string
+  }))
+  default = {
+    web = {
+      cores=2
+      memory=2
+      core_fraction=5
+      hdd_size=10
+      hdd_type="network-hdd"
+    }
+    storage = {
+      cores=2
+      memory=1
+      core_fraction=5
+      hdd_size=5
+      hdd_type="network-hdd"
+    }
+  }
 }
